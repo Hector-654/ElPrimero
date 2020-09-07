fe93f40d54b95e77c45c0c054965767a5b7a9eae
Hector-654 <69879591+Hector-654@users.noreply.github.com>
2020-09-06 20:54:33 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,17 +1,52 @@
 //https://www.eclipse.org/paho/clients/js/
 
 function LED1_On() {
-	alert("led on");
+	//alert("led on");
 	console.log("led on");
-	document.getElementById("sensor").innerHTML="led on";
+	//document.getElementById("sensor").innerHTML="led on";
+	message =new Paho.MQTT.Message("LED1_ALTO");
+	message.destinationName = "654hector1@gmail.com/kop";
+	client.send(message);
   
 }
 function LED1_Off(){	
-	alert("led off");
+	//alert("led off");
 	console.log("led off");
-	document.getElementById("sensor").innerHTML="led off";
+	//document.getElementById("sensor").innerHTML="led off";
+	message =new Paho.MQTT.Message("LED1_bajo");
+	message.destinationName = "654hector1@gmail.com/kop";
+	client.send(message);
 }
+function SUMA(){
+	var cd1 = parseFloat(document.getElementById('cd1').value);
+	var cd2 = parseFloat(document.getElementById('cd2').value);
+	var suma = cd1 + cd2;
+	console.log("la suma es:...."+suma);
+	document.getElementById('asw').value= suma;
+	//alert('la suma es :' +suma);
+		
+	message =new Paho.MQTT.Message(""+suma);
+	message.destinationName = "654hector1@gmail.com/kop";
+	client.send(message)
+		
+	
 
+}
+function RESTA(){
+	var cd1 = parseFloat(document.getElementById('cd1').value);
+	var cd2 = parseFloat(document.getElementById('cd2').value);
+	var resta = cd1 - cd2;
+	console.log("la resta es:...."+resta);
+	document.getElementById('asw').value= resta;
+	//alert('la suma es :' +suma);v
+		
+	message =new Paho.MQTT.Message(""+resta);
+	message.destinationName = "654hector1@gmail.com/kop";
+	client.send(message)
+		
+	
+
+}
 
 
 
@@ -27,8 +62,8 @@ function LED1_Off(){
   client.onMessageArrived = onMessageArrived;
   var options = {
    useSSL: false,
-    userName: "654hector1@gmail.com",
-    password: "naruto798199429",
+    userName: "lfrenteriax@hotmail.com",
+    password: "lfrenteriax",
     onSuccess:onConnect,
     onFailure:doFail
   }
@@ -41,9 +76,9 @@ function LED1_Off(){
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
 	
-    client.subscribe("654hector1@gmail.com/test");
+    client.subscribe("lfrenteriax@hotmail.com/test");
     message = new Paho.MQTT.Message("hola desde la web");
-    message.destinationName = "654hector1@gmail.com/kop";
+    message.destinationName = "lfrenteriax@hotmail.com/test1";
     client.send(message);
 	
   }
@@ -62,6 +97,6 @@ function LED1_Off(){
 
   // called when a message arrives
   function onMessageArrived(message) {
-    console.log("mensaje del sensor:"+message.payloadString);
+    console.log("onMessageArrived:"+message.payloadString);
   }
   
